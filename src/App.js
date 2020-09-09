1cfc314b2f18a2e61fb14ca0220133175d0f46fd
paradoxrevolver <adamtheball@gmail.com>
2020-09-09 02:09:25 -0700

    basic set up and layout of the app
    currently using figma to plan everything out, layout will likely change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div id="grid" className="page-cover">
+        <div id="menu-bar"></div>
+        <div id="left-col"></div>
+        <div id="main-col"></div>
+        <div id="right-col"></div>
+        <div id="bottom-bar"></div>
+      </div>
     </div>
   );
 }
